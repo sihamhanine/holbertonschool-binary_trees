f3ad7fdf868b06c19ad3815a9391119759a6487d
Souf <soufianmghirat@hotmail.com>
2024-04-10 15:14:36 +0200

    coorect the code

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,12 +6,7 @@
 */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node != NULL)
-	{
-		if (node->left != NULL && node->right != NULL)
-			return (1);
-		else
-			return (0);
-	}
+	if (node != NULL && node->parent == NULL)
+		return (1);
 	return (0);
 }
